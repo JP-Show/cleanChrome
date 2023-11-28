a753a9779c069c3ee99ed288ac7acb7d4618e515
Andre <andreluiz.jpofi@gmail.com>
2023-11-28 09:21:56 -0300

    push correct project

--- src/main/java/pom/Main.java
+++ src/main/java/pom/Main.java
@@ -10,43 +10,50 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v119.network.Network;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		String userName = System.getProperty("user.name");
-		String userPath = "C:\\Users\\"+ userName +"\\AppData\\Local\\Google\\Chrome\\User Data\\";
+		String userPath = "C:\\Users\\" + userName + "\\AppData\\Local\\Google\\Chrome\\User Data\\";
 		String currentDirectory = System.getProperty("user.dir");
 
-		Path path = Paths.get("C:\\Users\\" + userName + "\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1\\");
-		
-		WebDriverManager.chromedriver().setup();
-		//System.setProperty("webdriver.chrome.driver", currentDirectory + "\\chromedriver.exe");
+		Path path2 = Paths.get("C:\\Users\\" + userName + "\\AppData\\Local\\Google\\Chrome\\User Data\\");
 
-		System.out.println(currentDirectory + "\\chromedriver.exe");
-		
+		// WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver", currentDirectory + "\\chromedriver.exe");
+
 		ChromeOptions option = new ChromeOptions();
 		option.addArguments("user-data-dir=" + userPath);
-		
+
 		WebDriver driver = null;
 		DevTools devTools = null;
 		boolean isValid = false;
 		try {
 
-			File file = new File(path.toString());
+			File file = new File(path2.toString());
 			File[] files = file.listFiles();
 			for (File fil : files) {
-				if (fil.toString().contains("Login Data") || fil.toString().contains("History")) {
-					fil.delete();
-					System.out.println("entrou e deletou");
+				if(fil.toString().contains("Profile ")) {
+					deleteRecur(fil);
+				}
+				if (fil.toString().contains("Local State")) {
+					System.out.println("entrou no "+ fil.getName() +" e deletou " + fil.delete());
+				}
+				if (fil.toString().contains("Default")) {
+					File file2 = new File(path2.toString() + "\\" + fil.getName());
+					for (File fil2 : file2.listFiles()) {
+						if (fil2.toString().contains("History") || fil2.toString().contains("Login Data")) {
+							System.out.println("entrou no "+ fil2.getName() +" e deletou " + fil2.delete());
+						}
+					}
 				}
 			}
-
 			driver = new ChromeDriver(option);
 			devTools = ((ChromeDriver) driver).getDevTools();
 			isValid = true;
-			
+
 			devTools.createSession();
 
 			Thread.sleep(2000);
@@ -60,7 +67,7 @@ public class Main {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		} finally {
-			if(isValid) {
+			if (isValid) {
 				driver.close();
 				devTools.close();
 				driver.quit();
@@ -68,5 +75,20 @@ public class Main {
 			}
 		}
 	}
-
+	public static void deleteRecur(File file) {
+		if(file.isDirectory()) {
+			File[] files = file.listFiles();
+			if(file != null) {
+				for(File f : files) {
+					deleteRecur(f);
+				}
+			}
+		}
+		boolean deleted = file.delete();
+		if(deleted) {
+			System.out.println("deletou");
+		}else {
+			System.out.println("não deletou");
+		}
+	}
 }
